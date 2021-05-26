f87e78595f8916fb7275e02e5e726420a2306d7c
Sarbjit Singh <sarbjeets18@gmail.com>
2021-05-26 01:08:36 +0100

    hamburger menu added

--- js/logic.js
+++ js/logic.js
@@ -6,16 +6,25 @@ function pageStart(){
     console.log("page is loaded");
 }
 
-document.querySelector(".helpme").addEventListener("click",()=>{
-   let element1= document.createElement("p");
-   let inner_text= document.createTextNode("buton pressed first-time");
-   element1.appendChild(inner_text);
-   let divElement= document.getElementById("div1");
-   divElement.appendChild(element1);
-   console.log("Button is pressed");
-   setTimeout(() => {
-       divElement.removeAttributeNode("p")
+window.onload=()=>{
+console.log("default loop is running");
 
-   }, 5000);
+}
 
-}); 
+var ele =document.getElementsByTagName("*");
+console.log(ele);
+
+
+// document.querySelector(".helpme").addEventListener("click",()=>{
+//    let element1= document.createElement("p");
+//    let inner_text= document.createTextNode("buton pressed first-time");
+//    element1.appendChild(inner_text);
+//    let divElement= document.getElementById("div1");
+//    divElement.appendChild(element1);
+//    console.log("Button is pressed");
+//    setTimeout(() => {
+//        divElement.removeAttributeNode("p")
+
+//    }, 5000);
+
+// }); 
